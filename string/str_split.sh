98891d3453157d23a6b2b7d32d4f39c1a4e50b86
L <chinalihongde@163.com>
2017-07-05 10:33:05 +0800

    string test shell modify

--- string/str_split.sh
+++ string/str_split.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#用tr工具
+#用tr
 path=$(echo $PATH)
 path_split=$(echo $path | tr ":" "\n")
 for path_item in $path_split
@@ -7,6 +7,6 @@ do
 	echo $path_item
 done
 
-#用sed工具
+#用sed
 names="orange:apple:tomato:banana:pear"
 echo $names | sed 's/:/\n/g'
