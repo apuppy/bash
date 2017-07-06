42c4579bed04a39cbe0c33f8c5c72525572a2bb0
L <chinalihongde@163.com>
2017-07-06 10:07:06 +0800

    split string use IFS

--- string/str_split.sh
+++ string/str_split.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+
 #用tr
+
+echo -e "\n use sed \n"
+
 path=$(echo $PATH)
 path_split=$(echo $path | tr ":" "\n")
 for path_item in $path_split
@@ -8,5 +12,21 @@ do
 done
 
 #用sed
+
+echo -e "\n use sed \n"
+
 names="orange:apple:tomato:banana:pear"
 echo $names | sed 's/:/\n/g'
+
+#用IFS
+
+echo -e "\n use IFS \n"
+
+OIFS=$IFS
+ps=$PATH
+IFS=':'
+for pi in $ps
+do
+	echo "$pi"
+done
+IFS=$OIFS
